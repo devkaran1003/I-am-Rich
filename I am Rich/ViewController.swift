48cc3bd2f03ec8ae3d8b9b53e5c1f2eca84cb3d5
Dev <devkaransingh856@gmail.com>
2024-10-07 23:11:14 +0530

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Dev Karan Singh on 19/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
